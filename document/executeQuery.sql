eb74d1784fd442dda3da509c1c4110b63936ed8c
osumi <osumiyuki1123@gmail.com>
2023-07-21 00:37:03 +0900

    実行したクエリを追加する

---
+++ document/executeQuery.sql
@@ -0,0 +1,10 @@
+/* ユーザー情報テーブル作成 */
+create table user_info(user_id int,user_name varchar(20),user_password varchar(30));
+
+/* ユーザー情報テーブルの中身を確認 */
+select * from user_info;
+
+/* ユーザー情報にデータ追加 */
+insert into user_info values(1,'osumi','*19991123Oy');
+insert into user_info values(2,'yuuki','aaaaa');
+insert into user_info values(3,'atcoder','bbbbb');
