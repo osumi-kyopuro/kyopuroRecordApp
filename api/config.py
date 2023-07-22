7c9f73a6693ccc4331e93fd03d78052f6610273c
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2023-07-23 04:03:25 +0900

    #7 Clean config.py

--- api/config.py
+++ api/config.py
@@ -6,21 +6,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def getenv(env_name: str):
+def getenv(env_name: str) -> str:
     ret = os.getenv(env_name)
     if ret:
         return ret
-    raise RuntimeError("TODO")
+    raise RuntimeError(f"{env_name}が環境変数に登録されていません．")
 
 
 @dataclass(frozen=True)
 class DbConfig:
     """RDS接続情報"""
 
-    user: str
-    password: str
-    host: str
-    database: str
+    __user: str
+    __password: str
+    __host: str
+    __database: str
 
     @staticmethod
     def get() -> "DbConfig":
@@ -30,9 +30,3 @@ class DbConfig:
 
     def to_dict(self) -> dict:
         return asdict(self)
-
-
-if __name__ == "__main__":
-    d = DbConfig.get()
-    print(d)
-    print(d.to_dict())
