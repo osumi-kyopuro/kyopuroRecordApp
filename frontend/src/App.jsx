2289b4dce8dff7b94ccaaa46c42e51511c5ed28a
osumi <osumiyuki1123@gmail.com>
2023-09-22 03:59:20 +0900

    簡易的なheader完成

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+import {Footer} from "./components/Footer";
+import {Header} from "./components/Header";
+
 
 export const App=()=> {
   return (
-    <div>
-      kyopuroRecordApp
-    </div>
+    <>
+      <Header/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const style={
+    backgroundColor:"#000000",
+    color:"#ffffff",
+    width: "100%",
+    height: "50px"
+};
+
+
+export const Header=()=>{
+    return (
+        <>
+            <div style={style}>
+                kyopuroRecordApp
+            </div>
+        </>
+    );
+};
