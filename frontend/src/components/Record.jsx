2cc379c24c8117d73a82e8af9cb89cf57ccb24dc
osumi <osumiyuki1123@gmail.com>
2023-10-23 04:16:04 +0900

    削除確認モーダル作成

--- frontend/src/components/Record.jsx
+++ frontend/src/components/Record.jsx
@@ -2,6 +2,7 @@ import React, { useState ,useEffect} from "react";
 import { useNavigate } from 'react-router-dom';
 import "./recordStyle.css";
 import axios from "axios";
+import Modal from "react-modal";
 
 const box={
     position:"relative",
@@ -26,6 +27,47 @@ const insertButton={
     fontWeight:"bold"
 }
 
+const modalStyle = {
+    overlay: {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        backgroundColor: "rgba(0,0,0,0.50)"
+    },
+    content: {
+        position: "absolute",
+        top: "5rem",
+        left: "5rem",
+        right: "5rem",
+        bottom: "30rem",
+        backgroundColor: "paleturquoise",
+        borderRadius: "1rem",
+        padding: "1.5rem"
+    }
+};
+
+const modalYesButtonStyle={
+    cursor:"pointer",
+    backgroundColor:"red",
+    color:"white",
+    borderRadius:"10%",
+    fontSize:"200%",
+    fontWeight:"bold",
+    float: "right",
+    margin:"1%"
+}
+
+const modalNoButtonStyle={
+    cursor:"pointer",
+    backgroundColor:"gray",
+    color:"white",
+    borderRadius:"10%",
+    fontSize:"200%",
+    fontWeight:"bold",
+    float: "right",
+    margin:"1%"
+}
+
 export const columns = [
     { header: "カテゴリ", accessor: "category" },
     { header: "問題番号", accessor: "questionNumber" },
@@ -40,6 +82,9 @@ export const columns = [
 export const Record=(props)=>{
     const { userName,setSaveRecord} = props;
     const [record,setRecord]=useState([]);
+    const [deleteConfirmRecord,setDeleteConfirmRecord]=useState([]);
+    const [deleteId, setDeleteId] = useState(0);
+    const [modalIsOpen, setIsOpen] = useState(false);
     const navigate = useNavigate();
     useEffect(() => {
         const apiUrl='http://0.0.0.0/api/record/user/'+userName;
@@ -66,6 +111,20 @@ export const Record=(props)=>{
             });
     }
 
+    const deleteConfirm=(id)=>{
+        const apiUrl="http://0.0.0.0/api/record/id/"+id;
+        axios.get(apiUrl)
+            .then(function (res) {
+                console.log(res.data);
+                setDeleteConfirmRecord(res.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        setDeleteId(id);
+        setIsOpen(true);
+    }
+
     const deleteRecord=(id)=>{
         const apiUrl="http://0.0.0.0/api/record/id/"+id;
         console.log(id);
@@ -79,12 +138,48 @@ export const Record=(props)=>{
             .catch(function (error) {
                 console.log(error);
             });
+        initDeleteModal();
+    }
+
+    const initDeleteModal=()=>{
+        setIsOpen(false);
+        setDeleteId(0);
     }
 
     return (
         <div style={box}>
             <h1>精進記録</h1>
             <button style={insertButton} onClick={()=>{initRecord()}}>新規登録</button>
+            <Modal isOpen={modalIsOpen} style={modalStyle}>
+                <h1>本当に削除しますか？</h1>
+                <table border="2">
+                    <tr>
+                    {
+                        columns.map((item)=>{
+                            return(
+                                <th>{item.header}</th>
+                            )
+                        })
+                    }
+                    </tr>
+                    {
+                        
+                        <tr>
+                            <td>{deleteConfirmRecord.category}</td>
+                            <td>{deleteConfirmRecord.question_number}</td>
+                            <td>{deleteConfirmRecord.question_name}</td>
+                            <td>コードリンク</td>
+                            <td>{deleteConfirmRecord.memo}</td>
+                            <td>{deleteConfirmRecord.ac_day}</td>
+                            <td>編集</td>
+                            <td>削除</td>
+                        </tr>
+                        
+                    }
+                    </table>
+                <button style={modalNoButtonStyle} onClick={() => initDeleteModal()}>NO</button>
+                <button style={modalYesButtonStyle} onClick={() => deleteRecord(deleteId)}>YES</button>
+            </Modal>
                     
             <table border="2">
                 <tr>
@@ -107,7 +202,7 @@ export const Record=(props)=>{
                                 <td>{item.memo}</td>
                                 <td>{item.ac_day}</td>
                                 <td><button style={editDeleteButton} onClick={()=>{editRecord(item.record_id)}}>編集</button></td>
-                                <td><button style={editDeleteButton} onClick={()=>{deleteRecord(item.record_id)}}>削除</button></td>
+                                <td><button style={editDeleteButton} onClick={()=>{deleteConfirm(item.record_id)}}>削除</button></td>
                             </tr>
                         )
                     })
